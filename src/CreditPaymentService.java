0c6eeb66b48a1d01ed9704937399b41542ce73a2
Ekaterina <shi.e.m@yandex.ru>
2023-08-22 14:46:02 +0300

    Second commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,9 +2,10 @@ public class CreditPaymentService {
     public double calculate(int a, int b, double c) {
         int loanAmount = a; //сумма кредита
         int numberOfMonths = b;  //  кол-во месяцев
-        double bet = c;   // % ставка в месяц
-        double d = 1 + c;  // скобка
-        int result = (int) (a * (c + (c / (Math.pow(d, b) - 1))));
+        double e = c/12/100;
+        double bet = c;   // % ставка в год
+        double d = 1 + e;  // скобка
+        int result = (int) (a * (e + (e / (Math.pow(d, b) - 1))));
         return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         CreditPaymentService service = new CreditPaymentService();
-        int cps = (int) service.calculate(1_000_000, 12, 0.008325);
+        int cps = (int) service.calculate(1_000_000, 12, 9.99);
         System.out.println(cps);
-        int cps2 = (int) service.calculate(1_000_000, 24, 0.008325);
+        int cps2 = (int) service.calculate(1_000_000, 24, 9.99);
         System.out.println(cps2);
-        int cps3 = (int) service.calculate(1_000_000, 36, 0.008325);
+        int cps3 = (int) service.calculate(1_000_000, 36, 9.99);
         System.out.println(cps3);
 
     }
