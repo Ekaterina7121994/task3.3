b75f4f42f21ec5a322e113b6422a5954aefaf768
Ekaterina <shi.e.m@yandex.ru>
2023-08-25 10:40:53 +0300

    Сode correction

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public double calculate(int credit, int term, double annualRate) {
-        double ratePerMonth = annualRate/12/100;
+        double ratePerMonth = annualRate / 12 / 100;
         double bracket = 1 + ratePerMonth;
         int result = (int) (credit * (ratePerMonth + (ratePerMonth / (Math.pow(bracket, term) - 1))));
         return result;
